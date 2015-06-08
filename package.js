fd138a92f6c7967033823b75ecc40177884a69a6
selaias <s.elaias@gmail.com>
2015-06-08 08:49:01 +0000

    version bump

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'selaias:accounts-strava',
-  version: '0.1.1',
+  version: '0.2.0',
   summary: 'Login service for Strava accounts',
   git: 'https://github.com/selaias/accounts-strava.git',
   documentation: 'README.md'
@@ -13,7 +13,7 @@ Package.onUse(function(api) {
   api.imply('accounts-base', ['client', 'server']);
   api.use('accounts-oauth', ['client', 'server']);
   
-  api.use('selaias:strava@0.1.1', ['client', 'server']);
+  api.use('selaias:strava@0.2.0', ['client', 'server']);
   
   api.addFiles("strava.js");
 });
